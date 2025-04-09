03fcb066f92e2b135d8915e836d2195e8c55213d
Mathura <Torque-Lab@users.noreply.github.com>
2025-04-09 18:53:51 +0530

    random function for  ID

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,18 @@ import cors from "cors";
 
 const app =express ();
 app.use(cors());
+app.use(express.json());
 
+app.post("/deploy",(req,res)=>{
 
+const repoUrl=req.body.repoUrl
+
+console.log(repoUrl);
+res.json({
+
+})
+
+})
 
 
 app.listen(3000);
